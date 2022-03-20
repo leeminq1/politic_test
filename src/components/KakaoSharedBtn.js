52457041f9c06c0c01be4de894193ee89309e630
leeminq1 <70461938+leeminq1@users.noreply.github.com>
2022-03-20 12:41:25 +0900

    Update KakaoSharedBtn.js

--- src/components/KakaoSharedBtn.js
+++ src/components/KakaoSharedBtn.js
@@ -34,7 +34,7 @@ const KakaoShareBtn = ({name}) => {
       // 중복 initialization 방지
       if (!kakao.isInitialized()) {
         // 두번째 step 에서 가져온 javascript key 를 이용하여 initialize
-        kakao.init("6a65244ba444cbcf5d68ca0dba13c6a6");
+        kakao.init("해당하는 id값 사용");
       }
       kakao.Link.createDefaultButton({
         // Render 부분 id=kakao-link-btn 을 찾아 그부분에 렌더링을 합니다
